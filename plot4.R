31ef35e7258b98ec552f27de27d3bf0373559359
csmateixeira <csma.teixeira@gmail.com>
2014-10-09 19:52:42 +0100

    added plot4.R and plot4.png

---
+++ plot4.R
@@ -0,0 +1,31 @@
+library(dplyr)
+
+if(!file.exists("data/household_power_consumption.zip"))
+    download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "data/household_power_consumption.zip", method = "curl")
+
+if(!file.exists("data/household_power_consumption.txt"))
+    unzip("data/household_power_consumption.zip", exdir = "data")
+
+data <- 
+    read.csv(file = "data/household_power_consumption.txt", na.strings = "?", header = TRUE, sep = ";") %>%    
+    transform(FullDate = strptime(paste(Date, Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")) %>%
+    mutate(Date = as.Date(Date, format = "%d/%m/%Y")) %>%
+    filter(Date %in% c(as.Date("2007-02-01", format = "%Y-%m-%d"), as.Date("2007-02-02", format = "%Y-%m-%d"))) %>%    
+    transform(Day = weekdays(FullDate, abbreviate = TRUE))
+
+png(filename = "plot4.png", width = 480, height = 480)
+
+par(mfrow = c(2,2))
+
+with(data, {
+    plot(FullDate, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+    plot(FullDate, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+    plot(FullDate, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+    lines(FullDate, Sub_metering_1, col = "black")
+    lines(FullDate, Sub_metering_2, col = "red")
+    lines(FullDate, Sub_metering_3, col = "blue")
+    legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), bty = "n")
+    plot(FullDate, Global_reactive_power, type = "l", xlab = "datetime")
+})
+
+dev.off()
